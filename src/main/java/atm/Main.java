2e6f01fea415da4201d89722d5eee6667f79e449
Milk <milk@192.168.2.34>
2019-08-28 14:31:36 +0700

    change main and add xml

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -1,11 +1,23 @@
 package atm;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        DataSource dataSource = new DataSource("customers.txt");
-        Bank bank = new Bank(dataSource);
-        ATM atm = new ATM(bank);
-        ATMSimulator atmSimulator = new ATMSimulator(atm);
-        atmSimulator.run();
+//        DataSource dataSource = new DataSource("customers.txt");
+//        Bank bank = new Bank(dataSource);
+        //ATM atm = new ATM(bank);
+        // ATMSimulator atmSimulator = new ATMSimulator(atm);
+//        atmSimulator.run();
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("config.xml");
+        ATM ATM = context.getBean("bank",ATM.class);
+        DataSource dataSource = context.getBean("filename",DataSource.class);
+        Bank bank = context.getBean("dataSource" ,Bank.class);
+
+
+
+
     }
 }
